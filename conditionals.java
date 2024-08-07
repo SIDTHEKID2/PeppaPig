a7f1aa87af6730c8722fdd7d8e203b266aaa907a
SIDTHEKID2 <iamnotsidkarthi@gmail.com>
2024-08-07 19:48:00 -0400

    TIY conditionals

---
+++ conditionals.java
@@ -0,0 +1,12 @@
+public class conditionals {
+    public static void main(String[] args) {
+        int x = 70;
+        if (x < 100) {
+            x = x + 50;
+        }
+        if (x > 100) {
+            x = x / 20;
+        }
+        System.out.println(x);
+    };
+};
