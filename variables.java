92936c02cdd1be39d7b2d4df660ed64351b3c3a7
SIDTHEKID2 <iamnotsidkarthi@gmail.com>
2024-08-07 19:18:58 -0400

    variables TIY

---
+++ variables.java
@@ -0,0 +1,8 @@
+public class variables {
+    public static void main(String[] args) {
+        int mutable = 10;
+        final String IMMUTABLE = "hello";
+        System.out.println(mutable);
+        System.out.println(IMMUTABLE);
+    };
+};
