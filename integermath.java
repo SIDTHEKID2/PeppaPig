45973a76e714b169cc75ff5a6f0a30845ec912ce
SIDTHEKID2 <iamnotsidkarthi@gmail.com>
2024-08-07 19:26:08 -0400

    TIY integermath

---
+++ integermath.java
@@ -0,0 +1,10 @@
+public class integermath {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        x = x + 1;
+        y = y - 1;
+        int z = x + y;
+        System.out.println(z);
+    };
+};
