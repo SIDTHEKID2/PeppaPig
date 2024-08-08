efa7d60121f597d31c993057e7a894464d3a52aa
SIDTHEKID2 <iamnotsidkarthi@gmail.com>
2024-08-07 20:07:12 -0400

    TIY comments

---
+++ comments.java
@@ -0,0 +1,10 @@
+public class comments {
+  public static void main(String[] args) {
+      // I would rather be 11 feet tall
+
+    /* 
+        I've always been pretty short compared to others and I wanna experience being tall.
+        Also because it can give me a few advantages in specific activities and daily life in general.
+    */
+  };
+};
